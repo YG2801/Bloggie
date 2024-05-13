3d843772a0144ad35319bdd744fc8d3b8d1f7c6b
Yash Garg <yashgarg.6207@gmail.com>
2024-05-13 22:50:38 +0530

    rte bug fixed

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,38 +13,25 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 render={({ field: { onChange } }) => (
                     <Editor
                         apiKey={config.tinymceApiKey}
-                        initialValue={defaultValue}
                         init={{
-                            initialValue: defaultValue,
-                            height: 500,
-                            menubar: true,
-                            plugins: [
-                                "image",
-                                "advlist",
-                                "autolink",
-                                "lists",
-                                "link",
-                                "image",
-                                "charmap",
-                                "preview",
-                                "anchor",
-                                "searchreplace",
-                                "visualblocks",
-                                "code",
-                                "fullscreen",
-                                "insertdatetime",
-                                "media",
-                                "table",
-                                "code",
-                                "help",
-                                "wordcount",
-                                "anchor",
-                            ],
+                            plugins:
+                                "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
                             toolbar:
-                                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-                            content_style:
-                                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                                "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+                            tinycomments_mode: "embedded",
+                            tinycomments_author: "Author name",
+                            mergetags_list: [
+                                { value: "First.Name", title: "First Name" },
+                                { value: "Email", title: "Email" },
+                            ],
+                            ai_request: (request, respondWith) =>
+                                respondWith.string(() =>
+                                    Promise.reject(
+                                        "See docs to implement AI Assistant"
+                                    )
+                                ),
                         }}
+                        initialValue={defaultValue}
                         onEditorChange={onChange}
                     />
                 )}
